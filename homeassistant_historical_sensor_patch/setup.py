7a277e6f9e120a8192a5480b9380abd821a0dbd7
monforte10 <davidmonforteruiz@hotmail.com>
2026-03-25 15:40:07 +0100

    a

---
+++ homeassistant_historical_sensor_patch/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="homeassistant-historical-sensor-patch",
+    version="2.0.0.post1",
+    packages=["homeassistant_historical_sensor_patch"],  # detectará homeassistant_historical_sensor_patch
+    install_requires=[],
+)
