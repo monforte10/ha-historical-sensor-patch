d24d7ad222b6d77846c2f138dc3c5fe96c6a749a
monforte10 <davidmonforteruiz@hotmail.com>
2026-03-25 15:27:09 +0100

    a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="homeassistant-historical-sensor-patch",  # nombre del paquete en PyPI
-    version="2.0.0-patch1",                         # tu versión
+    version="2.0.0.dev1",                         # tu versión
     description="Fork parcheado de homeassistant-historical-sensor",
     url="https://github.com/monforte10/ha-historical-sensor-patch",
     author="Tu Nombre",
